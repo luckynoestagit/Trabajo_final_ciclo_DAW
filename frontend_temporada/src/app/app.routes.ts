fd726d5e9f195995527eaffaa66ccba08cd78595
luckynoesta <luc14bhz@hotmail.com>
2026-02-11 13:58:12 +0100

    Introduccion Header/Footer y actualizacion de app routes

--- frontend_temporada/src/app/app.routes.ts
+++ frontend_temporada/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./layouts/main-layout/main-layout').then(c => c.MainLayout),
+
+  }
+];
