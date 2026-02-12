7755368a4c69b9ca7ed12c5160fef5ee0ff22669
luckynoesta <luc14bhz@hotmail.com>
2026-02-12 14:13:58 +0100

    Cambios Frontend

--- frontend_temporada/src/app/app.routes.ts
+++ frontend_temporada/src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
+import {PaginaPrincipal} from './features/pagina-principal/pagina-principal';
 
 export const routes: Routes = [
   {
     path: '',
     loadComponent: () => import('./layouts/main-layout/main-layout').then(c => c.MainLayout),
-
-  }
+    children: [
+      {path: "",loadComponent: () => import('./features/pagina-principal/pagina-principal').then(c => c.PaginaPrincipal),
+      }
+    ]
+  },
 ];
---
+++ frontend_temporada/src/app/features/pagina-principal/pagina-principal.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-pagina-principal',
+  imports: [],
+  templateUrl: './pagina-principal.html',
+  styleUrl: './pagina-principal.css',
+})
+export class PaginaPrincipal {
+
+}
